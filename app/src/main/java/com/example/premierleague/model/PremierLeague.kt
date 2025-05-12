1118a596665ab1192f1647eb7b917c9054b239c0
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2025-05-12 22:40:41 +0100

    feat: implemented app logo

--- app/src/main/java/com/example/premierleague/model/PremierLeague.kt
+++ app/src/main/java/com/example/premierleague/model/PremierLeague.kt
@@ -3,11 +3,11 @@ package com.example.premierleague.model
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
-data class PremierLeague(
+data class PremierLeagueTeam(
     val id: Int,
     @StringRes val teamTitle: Int,
     @StringRes val teamSubtitle: Int,
-    @StringRes val teamStartDate: Int,
+    @StringRes val teamEstablished: Int,
     @StringRes val teamDetails: Int,
     @DrawableRes val imageResourceId: Int,
     @DrawableRes val teamImageBanner: Int
