a44356bf8c2d749de0d2972e2f4e96666a4841c1
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2025-05-22 20:51:31 +0100

    feat: implemented Screen and Navigation

--- app/src/main/java/com/example/premierleague/screens/PremierLeagueApp.kt
+++ app/src/main/java/com/example/premierleague/screens/PremierLeagueApp.kt
@@ -4,6 +4,7 @@ import android.R.attr.text
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,10 +14,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -67,8 +70,16 @@ fun PremierLeagueApp() {
                         }
 
                         Screen.Detail.route -> {
-                            Text("Show Details")
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                IconButton(onClick = { navController.popBackStack() }) {
+                                    Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                                }
+                                Text("Show Details")
+                            }
                         }
+
 
                         else -> "TV Show App"
                     }
